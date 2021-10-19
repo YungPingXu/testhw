e833af91dcea5e1ddff9ff678ada689ac458da8a
YungPingXu <a0976373843@gmail.com>
2021-10-19 20:29:21 +0800

    commit

--- ans.sh
+++ ans.sh
@@ -27,7 +27,7 @@ MainSelect(){
 }
 
 UserList(){
-    users=`
+	users=`
 		awk -F':' '{if(index($NF, "nologin") == 0){printf $1 " "}}' /etc/passwd | \
 		awk -v FS="\n" '{split($1, arr, " ")}END{for(i=1;i<=length(arr);i++){print 1000+i "," arr[i]}}'
 	`
@@ -45,11 +45,20 @@ UserList(){
 			fi
 		done
 	)
-    dialog --cancel-label "EXIT" --ok-label "SELECT"\
-    --menu "User Info Panel" 15 40 10 $para
+    choice=$(dialog --cancel-label "EXIT" --ok-label "SELECT" \
+    --menu "User Info Panel" 15 40 10 $para \
+    2>&1 > /dev/tty)
+    choice=$(($choice-1000))
+    
+    tmp=`
+    	cat /etc/passwd | grep ":" | grep -v "#" | grep -v "nologin" | \
+		awk -F':' '{printf $1 " "}'
+	`
+	username=$(echo "$tmp" | awk -F"\n" '{split($0,arr," ");print arr['$choice']}')
+
 	result=$?
     if [ $result -eq 0 ]; then # 0 means OK, 1 means cancel
-        UserVagrant
+        UserVagrant $username
     elif [ $result -eq 1 ] ; then
         Main
     fi
@@ -58,6 +67,76 @@ UserList(){
 Exit(){
     clear
     exit
+}
+
+UserVagrant(){
+	username=$1
+	
+    choice=$(dialog --cancel-label "EXIT" \
+    --menu "User vagrant" 15 40 10\
+    1 "LOCK IT" \
+    2 "GROUP INFO" \
+    3 "PORT INFO" \
+    4 "LOGIN HISTORY" \
+    5 "SUDO LOG" \
+    2>&1 > /dev/tty)
+    
+    result=$?
+    if [ $result -eq 0 ]; then # 0 means OK, 1 means cancel
+        UserVagrantSelect $choice $username
+    elif [ $result -eq 1 ] ; then
+        UserList
+    fi
+}
+
+UserVagrantSelect(){
+	choice=$1
+	username=$2
+	clear
+    case $choice in
+        1) echo option1
+        ;;
+        2) GroupInfo $username
+        ;;
+    esac
+}
+
+GroupInfo(){
+	username=$1
+	content=`
+		id $username |
+		awk '{
+			split(substr($NF,8),arr,","); \
+			for(i=1;i<=length(arr);i++){ \
+				print substr(arr[i], 1, length(arr[i])-1) \
+			} \
+		}' |
+		awk -F"(" '{print $1 " " $2}'
+	`
+    dialog --title "GROUP" \
+    --yes-label "OK" --no-label "EXPORT" \
+    --yesno "$content" 15 40
+	result=$?
+	if [ $result -eq 0 ]; then
+		UserVagrant
+	elif [ $result -eq 1 ] ; then
+		GroupExport $username "$content"
+	fi
+}
+
+GroupExport(){
+	username=$1
+	content=$2
+	exec 3>&1
+    input=$(dialog --title "Export to file" --inputbox "Enter the path:" 10 40 2>&1 1>&3)
+    result=$?
+    exec 3>&-
+	if [ $result -eq 0 ]; then
+		echo "$content" > "$input"
+		GroupInfo
+	elif [ $result -eq 1 ] ; then
+		GroupInfo
+	fi
 }
 
 PostAnnouncement(){
@@ -73,22 +152,6 @@ TypeMessage(){
     --inputbox "Enter your messages:" 10 40 
 }
 
-UserVagrant(){
-    dialog --cancel-label "EXIT" \
-    --menu "User vagrant" 15 40 10\
-    1 "LOCK IT" \
-    2 "GROUP INFO" \
-    3 "PORT INFO" \
-    4 "LOGIN HISTORY" \
-    5 "SUDO LOG"
-    result=$?
-    if [ $result -eq 0 ]; then # 0 means OK, 1 means cancel
-        Exit
-    elif [ $result -eq 1 ] ; then
-        UserList
-    fi
-}
-
 LockIt(){
     dialog --title "LOCK IT" \
     --yesno "Are you sure you want to do this?" 15 40
@@ -97,17 +160,6 @@ LockIt(){
 LockSucceed(){
     dialog --title "LOCK IT" \
     --msgbox "LOCK SUCCEED!" 15 40
-}
-
-ExportToFile(){
-    dialog --title "Export to file" \
-    --inputbox "Enter the path:" 10 40 
-}
-
-Group(){
-    dialog --title "GROUP" \
-    --yes-label "OK" --no-label "EXPORT" \
-    --yesno "content" 15 40
 }
 
 PortInfo(){
