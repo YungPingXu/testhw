5398298876f0e25b31866418b9b014c4d74fd828
YungPingXu <a0976373843@gmail.com>
2021-10-20 11:38:46 +0800

    commit

--- date.sh
+++ date.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-#datediff(){
-#   d1=$(date -d "$1" +%s)
-#   d2=$(date -d "$2" +%s)
-#   echo $(( (d1 - d2) / 86400 ))
-#}
-
-#datediff now "$date"
 date -j -f "%b %d %T" "Oct 20 11:36:00" "+%s"
 date "+%s"
