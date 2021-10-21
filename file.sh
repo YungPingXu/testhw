2feb7bc30e9050761f10188f820afc0e7942d3eb
YungPingXu <a0976373843@gmail.com>
2021-10-21 15:37:27 +0800

    commit

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+input="~/asd.txt"
+tmp=$(echo $input | cut -c1)
+if [ $tmp = "~" ] ; then
+	replace=$(echo "$input" | sed "s/^.\(.*\)/\1/")
+	path=$(echo $HOME $replace
+else
+	path=$input
+fi
+
+echo $path
+
+echo "content > $path
